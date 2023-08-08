606879e9391b7e392f7acb5147b209b69a3498fd
Juan Pintos <juanpintoselso33@gmail.com>
2023-08-08 16:22:38 +0000

    Tercer commit

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -4,12 +4,12 @@ function CountdownTimer(props) {
     const timeString = String(props.seconds).padStart(6, '0');
 
     return (
-      <div className="d-flex bg-dark text-white p-3 justify-content-center">
-        <div className="bg-secondary rounded p-2 mr-2 mx-3 d-flex align-items-center justify-content-center" style={{ width: '40px', height: '40px' }}>
+      <div className="d-flex bg-dark text-white p-4 justify-content-center fw-bolder fs-3">
+        <div className=" border border-1 bg-secondary rounded p-3 mr-2 mx-3 d-flex align-items-center justify-content-center" style={{ width: '40px', height: '50px' }}>
           <i className="fa-regular fa-clock" style={{color: "#ffffff"}}></i>
         </div>
         {timeString.split('').map((digit, index) => (
-          <div key={index} className="bg-secondary rounded p-2 mr-2 mx-3 d-flex align-items-center justify-content-center" style={{ width: '40px', height: '40px' }}>
+          <div key={index} className=" border border-1 bg-secondary rounded p-3 mr-2 mx-3 d-flex align-items-center justify-content-center" style={{ width: '40px', height: '50px' }}>
             {digit}
           </div>
         ))}
